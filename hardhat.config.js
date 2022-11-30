b1e72e52385b16e2b02ed99b922d4e8586692160
Yonko <95299919+kinuthier@users.noreply.github.com>
2022-11-30 23:01:46 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
   networks: {
     mumbai: {
       url: "https://old-blue-frog.matic-testnet.discover.quiknode.pro/c8c34aaf233ddca65ccc3b029926eaadf3c9503b/",
-      accounts: ["809bafc538f96ee1f93016a29714bee6d39bf26022d5945bbda9d1c27e22a408"],
-    }
-  }
+      accounts: ["809bafc538f96ee1f93016a29714bee6d39bf26022d5945bbda9d1c27e22a408"]
+    },
+    mainnet: {
+      chainId: 1,
+      url: "https://old-blue-frog.matic-testnet.discover.quiknode.pro/c8c34aaf233ddca65ccc3b029926eaadf3c9503b/",
+      accounts: ["809bafc538f96ee1f93016a29714bee6d39bf26022d5945bbda9d1c27e22a408"]
+    },
+  },
 };
